92fe38a044eea0bf7fc3fb766593e657a84b2c6d
ZqhKHP <G00336837@gmit.ie>
2017-11-03 15:04:45 +0000

    adding html file and creating a form for user input

--- ChatBot.go
+++ ChatBot.go
@@ -7,24 +7,57 @@
 package main
 
 import (
-	
-	"fmt"
+	"html/template"//add html/template package 
 	"net/http"
-	
-	
+
+//	"bytes"
 )
-
-func requestHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-
-	fmt.Fprintln(w, "Guessing Game")
-	//http.ServeFile(w, r, "3rdProblem/index.html")
-
+	
+type myMsg struct {
+	Input string
+	Output string
+	Previous string
 }
 
-//main method
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+	//serve the homepage.html file
+	http.ServeFile(w, r, "ChatBot.html")
+}
+
+func chatHandler(w http.ResponseWriter, r *http.Request) {
+
+	//create and initialise string
+	output		:= "Howya lad"
+	input		:= r.FormValue("chat")
+	previous	:= input
+	
+//	// checking for chat URL encoded variable
+//	input, err := r.URL.Query()["guess"]
+//	// if not found execute the template and exit
+//	if !err || len(guess) < 1 {
+//		log.Println("Url Param 'guess' is missing")
+//		// execute the template with the message
+//		tmpl.Execute(w, m)
+//		return
+//	}// if
+	
+	// Query()["guess"] will return an array of items, 
+	// we only want the single item.
+//	g := guess[0]
+	
+		// adding the guess value to the user value
+	//read the contents of chat.html and return a template
+	t, _ := template.ParseFiles("chat.html")
+
+	//execute template and pass pointer to myMsg 	struct
+	t.Execute(w, &myMsg{Input:input,Output:output,Previous:previous})
+}//chatHandler
+
 func main() {
-	//http.HandleFunc("/guess", requestHandler)
+	// handles root page
 	http.HandleFunc("/", requestHandler)
+
+	//handle /chat page
+	http.HandleFunc("/chat", chatHandler)
 	http.ListenAndServe(":8080", nil)
 }

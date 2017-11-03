d7abfc46071289949d2eec61ff260be015341a1a
ZqhKHP <G00336837@gmit.ie>
2017-11-03 14:55:51 +0000

    adding my basic go web file

---
+++ ChatBot.go
@@ -0,0 +1,30 @@
+// An echo web application.
+// problem sheet 2
+
+// "fmt"
+//"net/http"
+
+package main
+
+import (
+	
+	"fmt"
+	"net/http"
+	
+	
+)
+
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	fmt.Fprintln(w, "Guessing Game")
+	//http.ServeFile(w, r, "3rdProblem/index.html")
+
+}
+
+//main method
+func main() {
+	//http.HandleFunc("/guess", requestHandler)
+	http.HandleFunc("/", requestHandler)
+	http.ListenAndServe(":8080", nil)
+}

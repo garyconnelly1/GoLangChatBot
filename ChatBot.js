95df47d39f6e58667406473dcfa59236d0e1c9ab
ZqhKHP <G00336837@gmit.ie>
2017-11-19 13:09:25 +0000

    first attempt at adding GET method

--- ChatBot.js
+++ ChatBot.js
@@ -1,5 +1,5 @@
 const form = $("#userInput");
-//const listItem = $(".list-group-item");
+const listItem = $(".list-group-item");
 
 //add the keypress function for when the user types an input for eliza to compute
 form.keypress(function(event){
@@ -9,5 +9,15 @@ form.keypress(function(event){
     }
 
       event.preventDefault(); 
+
+      const input = form.val();
+      form.val(" ");
+
+      //using GET method to get the form value data from the html form
+      $.get('/chat', { value: form.val() } )
+            .done(function (data) {
+              $('listItem').val(data);
+              });
+      
 
  });

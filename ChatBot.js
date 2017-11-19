fd638ff845ac0e98f9c82c6e4fbb709c0a45fd3a
ZqhKHP <G00336837@gmit.ie>
2017-11-19 12:49:44 +0000

    adding first keypress function

--- ChatBot.js
+++ ChatBot.js
@@ -1,1 +1,13 @@
 const form = $("#userInput");
+//const listItem = $(".list-group-item");
+
+//add the keypress function for when the user types an input for eliza to compute
+form.keypress(function(event){
+    //for enter
+    if(event.keyCode != 13){ 
+        return;
+    }
+
+      event.preventDefault(); 
+
+ });
